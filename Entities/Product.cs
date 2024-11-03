6c07ea48f3a9985ef98744123b914ead87bb3686
Abbas Kimsani <abbas18679@gmail.com>
2024-11-03 18:23:46 +0100

    project init

---
+++ Entities/Product.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ProductManagerApi.Entities;
+
+public class Product
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Required]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Name must be between 5 and 100 characters.")]
+    public string Name { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
+    [Required]
+    public decimal Price { get; set; }
+    public bool Available { get; set; }
+    [StringLength(150, ErrorMessage = "Description must be maximum 150 characters.")]
+    public string Description { get; set; }
+    [Column(TypeName = "timestamp")]
+    public DateTime DateCreated { get; set; } = new();
+    [Timestamp]
+    public byte[] RowVersion { get; set; }
+}
---
+++ ProductManagerApiContext.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using ProductManagerApi.Entities;
+
+namespace ProductManagerApi;
+
+public class ProductManagerApiContext : DbContext
+{
+    public ProductManagerApiContext(){}
+    public ProductManagerApiContext(DbContextOptions<ProductManagerApiContext> options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(builder =>
+        {
+            builder.HasIndex(x => x.Name).IsUnique();
+            builder.Property(x => x.RowVersion)
+                .IsRowVersion();
+        });
+    }
+}
