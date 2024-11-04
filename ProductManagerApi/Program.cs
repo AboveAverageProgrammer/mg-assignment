38990983e3a3b72e0c334f3ddcefad1cb87fa47b
Abbas Kimsani <abbas18679@gmail.com>
2024-11-04 16:28:56 +0100

    added custom endpoint

--- ProductManagerApi/Program.cs
+++ ProductManagerApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using ProductManagerApi;
 using ProductManagerApi.Middleware;
+using ProductManagerApi.Models;
 using ProductManagerApi.Repositories;
 using ProductManagerApi.Services;
 
@@ -26,14 +27,34 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 var apiGroup = app.MapGroup("/api/v1");
 var productApiGroup = apiGroup.MapGroup("/product");
 
 app.UseHttpsRedirection();
-apiGroup.MapGet("/public", [AllowAnonymous]() => Results.Ok("This is a public endpoint that does not require authentication."));
+apiGroup.MapGet("/public",
+    [AllowAnonymous]() => Results.Ok("This is a public endpoint that does not require authentication."));
 
 apiGroup.MapGet("/protected", () => Results.Ok("This is a protected endpoint that requires authentication."));
 
-productApiGroup.MapGet("/",[AllowAnonymous](IProductService productService) => Results.Ok(productService.GetProductListAsync()));
+productApiGroup.MapGet("/",
+    [AllowAnonymous](IProductService productService) => Results.Ok(productService.GetProductListAsync()));
+
+productApiGroup.MapGet("/{id}", [AllowAnonymous]async(int id,IProductService productService) =>
+{
+    try
+    {
+        return Results.Ok(await productService.GetProductByIdAsync(id));
+    }
+    catch (EntityNotFoundException e)
+    {
+        return Results.NotFound(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        return Results.BadRequest(e.ToString());
+    }
+});
 
 app.Run();
