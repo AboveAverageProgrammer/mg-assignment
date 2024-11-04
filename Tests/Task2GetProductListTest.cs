b672f8f0395983083084d8e44e9bcc97c0c7911c
Abbas Kimsani <abbas18679@gmail.com>
2024-11-04 15:50:18 +0100

    detailed test

--- Tests/Task2GetProductListTest.cs
+++ Tests/Task2GetProductListTest.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata;
 using ProductManagerApi.Repositories;
 using ProductManagerApi.Services;
 
@@ -10,5 +11,11 @@ public class Task2GetProductListTest : ProductServiceBase
     {
         var result = _productService.GetProductListAsync().ToList();
         Assert.Equal(3, result.Count()); // 3 products are inserted in the database
+        Assert.Equal("Product 1", result[0].Name);
+        Assert.Equal("Product 2", result[1].Name);
+        Assert.Equal("Product 3", result[2].Name);
+        Assert.False(result[0].Available);
+        Assert.False(result[1].Available);
+        Assert.False(result[2].Available);
     }
 }
